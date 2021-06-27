ed97f9acd2bd211f270ed622b9644197bf85d190
Neil Bui <neilbui@westcliff.edu>
2021-06-26 18:44:28 -0700

    add build

---
+++ client/build/precache-manifest.5fc46b8b50a29d8a59754f2707fa9eb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "52e9ff4f5b97d965e505",
+    "url": "/static/css/main.32defc53.chunk.css"
+  },
+  {
+    "revision": "52e9ff4f5b97d965e505",
+    "url": "/static/js/main.52e9ff4f.chunk.js"
+  },
+  {
+    "revision": "386876b9a47ba0d76f70",
+    "url": "/static/css/1.4e478066.chunk.css"
+  },
+  {
+    "revision": "386876b9a47ba0d76f70",
+    "url": "/static/js/1.386876b9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "915e1884100b4b270522994aeadc82a6",
+    "url": "/index.html"
+  }
+];
