4e29291cb6ebb253ba2e4f3c85e001c373c5fd66
Neil Bui <neilbui@westcliff.edu>
2021-06-27 16:22:52 -0700

    prepared for heroku

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 
+const path = require('path');
+
 const users = require("./routes/api/users");
 
 const app = express();
@@ -14,6 +16,14 @@ app.use(
   })
 );
 app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // DB Config
 const db = require("./config/keys").mongoURI;
