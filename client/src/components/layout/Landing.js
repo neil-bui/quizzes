89410ac8505430c916efd317ce4e36f036827a2d
Neil Bui <neilbui@westcliff.edu>
2021-06-28 16:14:01 -0700

    made changes to homepage

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -8,13 +8,10 @@ class Landing extends Component {
         <div className="row">
           <div className="col s12 center-align">
             <h4>
-              <b>Build</b> a login/auth app with the{" "}
-              <span style={{ fontFamily: "monospace" }}>MERN</span> stack from
-              scratch
+              Welcome to Quiz You Quiz Me
             </h4>
             <p className="flow-text grey-text text-darken-1">
-              Create a (minimal) full-stack app with user authentication via
-              passport and JWTs
+              This is the Internet's next big shareable quiz for friends and family! Share your results on Facebook and Twitter!
             </p>
             <br />
             <div className="col s6">
